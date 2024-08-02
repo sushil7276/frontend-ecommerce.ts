b353f0a2d1ba0ed3b48da0caafc9f06609dd11ea
Babar <Sushil.Babar@turck.com>
2024-08-02 16:03:19 +0530

    Vercel Speed Insights Add

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles/app.scss";
 import { Provider } from "react-redux";
+import App from "./App.tsx";
 import { store } from "./redux/store.ts";
+import "./styles/app.scss";
+
+// Vercel Speed Insights
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
-      <Provider store={store}>
-         <App />
-      </Provider>
-   </React.StrictMode>
+   <Provider store={store}>
+      <App />
+      <SpeedInsights />
+   </Provider>
 );
